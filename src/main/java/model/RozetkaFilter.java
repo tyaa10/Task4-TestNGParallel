e65d9dfd65a72c26bbae28c8c9c718df124c2252
YuriiTrofimenko <tyaa@ukr.net>
2021-08-12 15:07:01 +0300

    dependencies update: JAXB

--- src/main/java/model/RozetkaFilter.java
+++ src/main/java/model/RozetkaFilter.java
@@ -1,7 +1,11 @@
 package model;
-import javax.xml.bind.annotation.XmlAccessType;
+/* import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlRootElement; */
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
 import java.io.Serializable;
 
 
--- src/main/java/util/XMLtoObject.java
+++ src/main/java/util/XMLtoObject.java
@@ -1,10 +1,13 @@
 package util;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import model.RozetkaFilters;
 
-import javax.xml.bind.JAXBContext;
+/* import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.Unmarshaller; */
 import java.io.File;
 
 public class XMLtoObject {
